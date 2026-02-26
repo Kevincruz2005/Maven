fdd6c9e21e46bd4c34ccdfac01bdd7baaefd53a2
Rohitamalraj <rohitamalraj@gmail.com>
2026-02-26 10:33:35 +0530

    first commit

---
+++ src/main/java/com/example/ogdemo/OgdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ogdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OgdemoApplication.class, args);
+	}
+
+}
